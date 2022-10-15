14e21f6f1bb01f422204b105ce4c09f7c0f213c8
JacopoG <jacopo.galardi@tutanota.com>
2022-10-15 19:39:57 +0200

    Add death on leaving layout or collision

--- Exploration/Snake/MainGame.gd
+++ Exploration/Snake/MainGame.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 var snake_body = [Vector2(7, 7)]
+var snake_head = snake_body[0]
 var last_direction
+var sliced_body
 
 var apple_pos
 var index = Vector2.ZERO
@@ -35,8 +37,9 @@ func make_snake():
 		$SnakeAppleMap.set_cell(part.x, part.y, 0, false, false, false, index)
 
 func _on_SnakeGameTick_timeout():
+	sliced_body = snake_body.slice(1, -1)
 	snake_body.append(Vector2(snake_body[-1].x + direction.x, snake_body[-1].y + direction.y))
-	print(apple_pos, snake_body)
+	snake_head = snake_body[0]
 	if apple_pos in snake_body:
 		pass
 		spawn_apple()
@@ -44,6 +47,10 @@ func _on_SnakeGameTick_timeout():
 		$SnakeAppleMap.set_cell(snake_body[0].x, snake_body[0].y, -1, false, false, false, index)
 		snake_body.remove(0)
 #		print(snake_body)
+	if snake_head in sliced_body or snake_head.x < 0 or snake_head.x > 14 or snake_head.y < 0 or snake_head.y > 14:
+		$SnakeGameTick.stop()
+#		get_tree().change_scene("res://MainGame.tscn")
+		print("DEAD") #TODO ADD DEATH
 	last_direction = direction
 	make_snake()
 
